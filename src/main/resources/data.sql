ab89bcb5c08e8ea2826d1ee757a08c0349faab16
FabianoTM <fabiano.tiengo@gmail.com>
2020-04-30 23:24:09 -0300

    Popolando BD para teste da paginação de clientes

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,14 @@
 INSERT INTO cliente (id, nome, email) VALUES
 (1, 'Mariazinha', 'mariazinha@email.com'),
-(2, 'Joãozinho', 'joaozinho@email.com');
+(2, 'Joãozinho', 'joaozinho@email.com'),
+(3, 'Eloá Elisa Ferreira', 'eloa@email.com'),
+(4, 'Thomas Vinicius Caio Drumond', 'Thomas@email.com'),
+(5, 'Sebastião Edson Peixoto', 'Sebastião@email.com'),
+(6, 'Andreia Luzia Carolina Fogaça', 'Andreia@email.com'),
+(7, 'Letícia Evelyn da Silva', 'Letícia@email.com'),
+(8, 'Regina Mirella Sophia Pereira', 'Regina@email.com'),
+(9, 'Diego Leonardo Thales Gomes', 'Diego@email.com'),
+(10, 'Henry Bernardo Mateus Rodrigues', 'Henry@email.com'),
+(11, 'Milena Emily Fernandes', 'Milena@email.com'),
+(12, 'Heitor Enzo Santos', 'Heitor@email.com'),
+(13, 'Calebe Renato Peixoto', 'Calebe@email.com');
